4d85d7840f4d380fde05a8ee00821cc13aaf1a5c
NadimKhalifeh <nadim_khalifeh@hotmail.com>
2023-12-03 04:04:02 -0500

    implementing the put function between the server and client

--- client.py
+++ client.py
@@ -1,6 +1,33 @@
 import socket
 import os
 
+put_opcode =0
+get_opcode = 1
+change_opcode = 2
+summary_opcode = 3
+help_opcode = 4
+typefile = "utf-8"
+
+def put_func(filename, client_socket):
+            if not os.path.exists(filename):
+                raise FileNotFoundError(f"File '{filename}' not found.")
+
+            if len(filename)>31:
+                raise("filename length should be 31 or less")
+            
+            
+            firstByte = command_byte(put_opcode, filename) 
+            client_socket.send(bytes([firstByte]))
+            client_socket.send(filename.encode(typefile)) 
+            filedata = open(filename, 'rb').read()
+            client_socket.send(filedata)
+
+
+def command_byte(opcode, filename):
+     msb = opcode << 5 
+     filename_length = len(filename)
+     return msb | filename_length
+
 def ftp_transfer_client(server_ip, server_port):
         while True:
             connection = input("Choose type of communication: TCP or UDP? ")
@@ -25,16 +52,26 @@ def ftp_transfer_client(server_ip, server_port):
             
             if connection == 'TCP':
                 # Send command to the server
-                client_socket.send(' '.join(command).encode())
+                # client_socket.send(' '.join(command).encode())
+                if(command[0].lower() == 'put'):
+                    print('transferring file')
+                    put_func(command[1], client_socket)
+                    break
+                elif(command[0].lower() == 'bye'):
+                    print('client terminated session')
+                    client_socket.close()
+                    break
+                
             elif connection == 'UDP':          
                 # Send command to the server
                 client_socket.sendto(' '.join(command).encode(),(server_ip,server_port))
-            if(command[0].lower() == 'bye'):
-                print('client terminated session')
-                client_socket.close()
-                break
+            
+    
+       
+                
+
                
     
 
 if __name__ == '__main__':
-    ftp_transfer_client('127.0.0.1',500)
+    ftp_transfer_client('127.0.0.1',2005)
--- server.py
+++ server.py
@@ -1,7 +1,29 @@
 import socket
 import os
 
+rescode_put =0 
+
+def decode_first_byte(first_byte):  # Add rescode to response message
+    new = int.from_bytes(first_byte, byteorder='big') 
+    rescode = new >>5
+    filename_length = new & 0x1F
+    return rescode, filename_length
+
+def receive_file(connection_socket, filename_length):
+    # Read the filename
+   encoded_filename = connection_socket.recv(filename_length)
+        # Decode the filename from bytes to a string
+   filename = encoded_filename.decode()
+    # Read the file data
+   file_data = connection_socket.recv(1024)
+    
+   with open("filename", 'wb') as file:
+        while file_data:
+            file.write(file_data)
+            file_data = connection_socket.recv(1024)
+
 def put_func(filename):
+
     pass
 
 def get_func(filename):
@@ -33,6 +55,7 @@ def fileTransferProtocol(port):
             #variable to know if there is a client currently connected
             connected = True
             print("TCP server launched!")
+            
             while True:
                 
                 if not(connected):
@@ -41,18 +64,25 @@ def fileTransferProtocol(port):
                     print(f'accepting connection from ip address: {addr}')
                     connected = True
 
-                message = connectionSocket.recv(1024).decode()
-
-                if message == 'bye' :
-                    # If client send bye, then server can close the connection
-                    bye()
-                    connectionSocket.close()
-                    connected = False
-                    continue 
-
+                first_byte = connectionSocket.recv(1)
+                if not first_byte:
+                     break
+                rescode, filename_length = decode_first_byte(first_byte)
+                
+                print(f"Decoded rescode: {rescode}, filename_length: {filename_length}")
+                # if message == 'bye' :
+                #     # If client send bye, then server can close the connection
+                #     bye()
+                #     connectionSocket.close()
+                #     connected = False
+                #     continue
+                if rescode == rescode_put :
+                    receive_file(connectionSocket, filename_length)
+                    print(f"File received successfully.")
+                
                 # Split the HTTP request sent into words
-                group_requests = message.split(" ")
-                print(group_requests)
+                #group_requests = message.split(" ")
+                #print(group_requests)
                 
         elif connection == 'UDP':     
             serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -72,4 +102,4 @@ def fileTransferProtocol(port):
     
 
 if __name__ == '__main__':
-    fileTransferProtocol(500)
+    fileTransferProtocol(2005)
